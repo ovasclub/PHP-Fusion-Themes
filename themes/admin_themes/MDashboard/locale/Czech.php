5e71c400b2928759d8717710f86e7c451ee5b9ae
robinn1 <robokelcak@centrum.sk>
2018-06-20 14:52:52 +0200

    Add locales

---
+++ themes/admin_themes/MDashboard/locale/Czech.php
@@ -0,0 +1,26 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: MDashboard/locale/Czech.php
+| Author: RobiNN
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['MD_001'] = 'Administrace';
+$locale['MD_002'] = 'Hledat...';
+$locale['MD_003'] = 'Vytvořil';
+// Result Messages
+$locale['MD_100'] = 'Selhal systém validace';
+$locale['MD_101'] = 'Selhal bezpečnostní systém opatření';
+$locale['MD_102'] = 'Stránky se nepodařilo načíst';
+$locale['MD_103'] = 'Hledaný výraz je příliš krátký';
+$locale['MD_104'] = 'Nebyly nalezeny žádné výsledky';
---
+++ themes/admin_themes/MDashboard/locale/Slovak.php
@@ -0,0 +1,26 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: MDashboard/locale/Slovak.php
+| Author: RobiNN
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['MD_001'] = 'Administrácia';
+$locale['MD_002'] = 'Hľadať...';
+$locale['MD_003'] = 'Vytvoril';
+// Result Messages
+$locale['MD_100'] = 'Zlyhal systém validácie';
+$locale['MD_101'] = 'Zlyhal bezpečnostný systém opatrení';
+$locale['MD_102'] = 'Stránky sa nepodarilo načítať';
+$locale['MD_103'] = 'Hľadaný výraz je príliš krátky';
+$locale['MD_104'] = 'Neboli nájdené žiadne výsledky';
