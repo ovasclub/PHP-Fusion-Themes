35a9d23753c3d8cad3a7c4285482c2ed719a7770
robinn1 <RobiNN1@users.noreply.github.com>
2018-10-28 11:48:01 +0100

    Locales

---
+++ themes/admin_themes/MDashboard/locale/Danish.php
@@ -0,0 +1,26 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: MDashboard/locale/Danish.php
+| Author: Helmuth Mikkelsen (helmuth)
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['MD_001'] = 'Administration';
+$locale['MD_002'] = 'Søg ...';
+$locale['MD_003'] = 'Oprettet af';
+// Result Messages
+$locale['MD_100'] = 'Mislykkede system validering';
+$locale['MD_101'] = 'Mislykkede system sikkerheds foranstaltninger';
+$locale['MD_102'] = 'Sider kunne ikke indlæses';
+$locale['MD_103'] = 'Søgestreng er for kort';
+$locale['MD_104'] = 'Der blev ikke fundet nogen resultater';
---
+++ themes/admin_themes/MDashboard/locale/Hungarian.php
@@ -0,0 +1,26 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: MDashboard/locale/Hungarian.php
+| Author: karrak
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['MD_001'] = "Adminisztráció";
+$locale['MD_002'] = "Keresés...";
+$locale['MD_003'] = "Készítette";
+// Result Messages
+$locale['MD_100'] = "Sikertelen rendszerellenőrzés";
+$locale['MD_101'] = "Sikertelen rendszerbiztonsági intézkedések";
+$locale['MD_102'] = "Nem sikerült betölteni az oldalakat";
+$locale['MD_103'] = "Kevés karaktert adtál meg";
+$locale['MD_104'] = "Nincs találat";
