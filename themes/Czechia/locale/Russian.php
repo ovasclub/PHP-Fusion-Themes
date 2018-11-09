7eb66155a258dabbf2332f6f2b1a49d2dd85e442
ovasclub <ovas.club@mail.ru>
2018-11-09 23:00:44 +0600

    Create Russian.php

---
+++ themes/Czechia/locale/Russian.php
@@ -0,0 +1,31 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: Czechia/locale/Russian.php
+| Author: ovasclub
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['pfc_title'] = 'Тема Чехия';
+$locale['pfc_description'] = 'Тема официальной чешской поддержки.';
+$locale['created_by']  = 'Создана';
+$locale['01'] = 'IP-адрес';
+$locale['02'] = 'О нас';
+$locale['03'] = 'Последние новости';
+$locale['04'] = 'Нет опубликованных новостей';
+$locale['05'] = 'Читать дальше';
+$locale['08'] = 'Категория';
+$locale['09'] = 'Загрузить';
+$locale['10'] = 'Описание издателя';
+$locale['11'] = 'Полный скриншот';
+$locale['12'] = '';
+$locale['13'] = 'Последние коментариии';
