559449e189fdc6b27d2e09be099a21949e7f005d
RobiNN <kelcakrobo@gmail.com>
2020-12-10 20:29:36 +0100

    Fix Material theme

--- themes/Material/classes/Main.php
+++ themes/Material/classes/Main.php
@@ -237,14 +237,14 @@ class Main extends Core {
 
                             echo '<ul class="dropdown-menu" aria-labelledby="dduser">';
                                 if (iADMIN) {
-                                    echo '<li><a href="'.ADMIN.'index.php'.fusion_get_aidlink().'&pagenum=0"><i class="fa fa-tachometer-alt"></i><small>'.$locale['global_123'].'</small></a></li>';
+                                    echo '<li><a href="'.ADMIN.'index.php'.fusion_get_aidlink().'&pagenum=0"><i class="fa fa-tachometer-alt"></i><small class="text-overflow-hide">'.$locale['global_123'].'</small></a></li>';
                                 }
 
-                                echo '<li><a href="'.BASEDIR.'messages.php"><i class="fa fa-envelope"></i><small>'.$locale['UM081'].'</small>'.$messages_count.'</a></li>';
-                                echo '<li><a href="'.BASEDIR.'edit_profile.php"><i class="fa fa-pen"></i><small>'.$locale['UM080'].'</small></a></li>';
-                                echo '<li><a href="'.BASEDIR.'profile.php?lookup='.$userdata['user_id'].'"><i class="fa fa-eye"></i><small>'.$locale['view'].' '.$locale['profile'].'</small></a></li>';
-                                echo '<li><a href="'.BASEDIR.'members.php"><i class="fa fa-users"></i><small>'.$locale['UM082'].'</small></a></li>';
-                                 echo '<li><a href="'.BASEDIR.'index.php?logout=yes"><i class="fa fa-sign-out-alt"></i><small>'.$locale['logout'].'</small></a></li>';
+                                echo '<li><a href="'.BASEDIR.'messages.php"><i class="fa fa-envelope"></i><small class="text-overflow-hide">'.$locale['UM081'].'</small>'.$messages_count.'</a></li>';
+                                echo '<li><a href="'.BASEDIR.'edit_profile.php"><i class="fa fa-pen"></i><small class="text-overflow-hide">'.$locale['UM080'].'</small></a></li>';
+                                echo '<li><a href="'.BASEDIR.'profile.php?lookup='.$userdata['user_id'].'"><i class="fa fa-eye"></i><small class="text-overflow-hide">'.$locale['view'].' '.$locale['profile'].'</small></a></li>';
+                                echo '<li><a href="'.BASEDIR.'members.php"><i class="fa fa-users"></i><small class="text-overflow-hide">'.$locale['UM082'].'</small></a></li>';
+                                 echo '<li><a href="'.BASEDIR.'index.php?logout=yes"><i class="fa fa-sign-out-alt"></i><small class="text-overflow-hide">'.$locale['logout'].'</small></a></li>';
                             echo '</ul>';
                         echo '</li>';
                     } else {
@@ -434,9 +434,11 @@ class Main extends Core {
 
         self::setParam($small.'header_content', $header);
 
-        if ($options['random_image'] == TRUE || $options['background'] !== NULL) {
-            $background = !empty($options['background']) && file_exists($options['background']) ? $options['background'] : self::getRandImg();
-            self::setParam('header_styles', 'background: url('.$background.')');
+        if ($options['background'] == TRUE || !empty($options['background'])) {
+            if ($options['random_image'] == TRUE || $options['background'] !== NULL) {
+                $background = !empty($options['background']) && file_exists($options['background']) ? $options['background'] : self::getRandImg();
+                self::setParam('header_styles', 'background: url('.$background.')');
+            }
         }
     }
 
