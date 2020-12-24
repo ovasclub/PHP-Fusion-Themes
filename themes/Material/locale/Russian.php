289e29a8211f111bd5a2f4f6c535dd2eec8e3e61
RobiNN <kelcakrobo@gmail.com>
2020-12-24 15:00:03 +0100

    Add Material Russian

---
+++ themes/Material/locale/Russian.php
@@ -0,0 +1,105 @@
+<?php
+/*-------------------------------------------------------+
+| PHPFusion Content Management System
+| Copyright (C) PHP Fusion Inc
+| https://www.phpfusion.com/
++--------------------------------------------------------+
+| Filename: Russian.php
+| Author: ovasclub
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['mt_title'] = 'Тема Материал';
+$locale['mt_description'] = 'Material Theme is inspired by Google Material Design.';
+$locale['readmore'] = 'Читать дальше';
+$locale['leave_comment'] = 'Оставить комментарий';
+
+// Main.php
+$locale['main_01'] = 'Светлый / Темный режим';
+$locale['main_02'] = 'Введите в поиск текст (не меньше 3-х знаков)';
+
+// AboutUs.php
+$locale['au_01'] = 'О нас';
+
+// ContactUs.php
+$locale['cu_01'] = 'Наши контакты';
+
+// Footer/LatestArticles.php
+$locale['la_01'] = 'Последнии статьи';
+$locale['la_02'] = 'Статьи еще не опубликованы';
+$locale['la_03'] = 'Модуль статей не установлен';
+
+// Footer/LatestBlogs.php
+$locale['lb_01'] = 'Последние блоги';
+$locale['lb_02'] = 'Блоги еще не опубликованы';
+$locale['lb_03'] = 'Модуль блогов не установлен';
+
+// Footer/LatestNews.php
+$locale['ln_01'] = 'Последние новости';
+$locale['ln_02'] = 'Новости еще не опубликованы';
+$locale['ln_03'] = 'Модуль новостей не установлен';
+
+// Footer/UsersOnline.php
+$locale['uo_01'] = 'Пользователи Online';
+$locale['uo_02'] = 'Сейчас на сайте [TOTAL_ONLINE] : [MEMBERS] и [GUESTS].';
+$locale['uo_03'] = 'Зарегестрированные';
+$locale['uo_04'] = 'никого';
+$locale['uo_05'] = 'Новый пользователь';
+$locale['uo_06'] = 'Сегодня сайт посещали';
+
+// Templates/Downloads.php
+$locale['dl_01'] = 'Категория';
+$locale['dl_02'] = 'Файлы';
+$locale['dl_03'] = 'Publisher\'s Description';
+$locale['dl_04'] = 'Подробные скриншоты';
+$locale['dl_05'] = 'от';
+
+// Templates/News.php
+$locale['ns_01'] = 'Связанные новости';
+
+// Templates/Profile.php
+$locale['profile_01'] = 'Изменить фото';
+$locale['profile_02'] = 'Фото обложки профиля';
+$locale['profile_03'] = 'Изменить фото обложки';
+$locale['profile_notice_01'] = 'Фото профиля было удалено';
+$locale['profile_notice_02'] = 'Фото профиля обновлено';
+$locale['profile_notice_03'] = 'Фото обложки профиля было удалено';
+$locale['profile_notice_04'] = 'Фото профиля обложки обновлено';
+
+// widget.php
+$locale['widget_title'] = 'Конфигурация темы Материал';
+$locale['widget_title_01'] = 'Основные настройки темы';
+$locale['widget_tab_01'] = 'Изменить социальную сеть';
+$locale['widget_tab_02'] = 'Добавить социальную сеть';
+$locale['widget_tab_03'] = 'Настройки темы';
+$locale['widget_01'] = 'Заголовок';
+$locale['widget_02'] = 'Иконка';
+$locale['widget_03'] = 'Ссылка';
+$locale['widget_04'] = 'Видима';
+$locale['widget_05'] = 'Показывать социальную сеть?';
+$locale['widget_06'] = 'Лого';
+$locale['widget_07'] = 'Имя сайта';
+$locale['widget_08'] = 'Нет';
+$locale['widget_09'] = 'Лого сайта';
+$locale['widget_10'] = 'Предосмотр';
+$locale['widget_11'] = 'Действия';
+$locale['widget_12'] = 'Социальные сети не определены. [link]Добавте сеть[/link].';
+
+$locale['widget_22'] = 'Скрыть';
+$locale['widget_23'] = 'Исключить панели нижнего колонтитула в';
+$locale['widget_24'] = 'Колонка нижнего колонтитула 1';
+$locale['widget_25'] = 'Колонка нижнего колонтитула 2';
+$locale['widget_26'] = 'Колонка нижнего колонтитула 3';
+$locale['widget_27'] = 'Колонка нижнего колонтитула 4';
+$locale['widget_notice_01'] = 'Социальная сеть добавлена.';
+$locale['widget_notice_02'] = 'Социальная сеть отредактирована.';
+$locale['widget_notice_03'] = 'Социальная сеть удалена.';
+$locale['widget_notice_04'] = 'Настройки темы обновлены.';
+
+$locale['widget_notice_08'] = 'Социальные сети переупорядочены.';
